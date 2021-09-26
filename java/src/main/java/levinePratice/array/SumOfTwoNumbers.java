1d30da71df89def608da83b8c4e93bc63ab6b925
zhouhuan <zhouhuan@100doc.com.cn>
2021-09-26 16:46:24 +0800

    [新增功能](master): 新增时间复杂度为O(N)的方法

--- java/src/main/java/levinePratice/array/SumOfTwoNumbers.java
+++ java/src/main/java/levinePratice/array/SumOfTwoNumbers.java
@@ -23,8 +23,10 @@ public class SumOfTwoNumbers {
         int target = 7;
         System.out.println(Arrays.toString(twoSum(nums, target)));
         System.out.println(Arrays.toString(twoSumByHash(nums, target)));
+        System.out.println(Arrays.toString(twoSum3(nums, target)));
     }
 
+    //时间复杂度n平方
         public static int[] twoSum(int[] nums, int target) {
             Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
             for (int i = 0; i < nums.length; ++i) {
@@ -45,6 +47,22 @@ public class SumOfTwoNumbers {
             hashtable.put(nums[i], i);
         }
         return new int[0];
+    }
+
+    public static int[] twoSum3(int[] nums, int target) {
+        int l = nums.length;
+        int[] ans=new int[2];
+        int i,j;
+        for (i = 0; i < l - 1; i++) {
+            for (j = i + 1; j < l; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
+
     }
 
 }
