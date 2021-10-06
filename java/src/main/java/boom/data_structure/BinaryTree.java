439e19ab6a1a4b8bbe6be5d20d1e5b1e83983628
ayb <13132512292@163.com>
2021-10-06 20:15:36 +0800

    '22:括号生成'

---
+++ java/src/main/java/boom/data_structure/BinaryTree.java
@@ -0,0 +1,47 @@
+package boom.data_structure;
+
+public class BinaryTree<T> {
+    private Node<T> root;
+
+    public Node<T> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<T> root) {
+        this.root = root;
+    }
+
+    public static class Node<T> {
+        private T val;
+        private Node left;
+        private Node right;
+
+        public Node(T val) {
+            this.val = val;
+        }
+
+        public T getVal() {
+            return val;
+        }
+
+        public void setVal(T val) {
+            this.val = val;
+        }
+
+        public Node<T> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<T> left) {
+            this.left = left;
+        }
+
+        public Node<T> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<T> right) {
+            this.right = right;
+        }
+    }
+}
