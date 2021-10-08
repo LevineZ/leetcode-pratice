3004dfb01880c9aaac59bc78f79b0e880d3b1bf0
ayb <13132512292@163.com>
2021-10-08 20:01:12 +0800

    '10：正则表达式匹配'

--- java/src/main/java/boom/hard/Hard10.java
+++ java/src/main/java/boom/hard/Hard10.java
@@ -1,7 +1,25 @@
 package boom.hard;
 
+import java.util.Arrays;
+
 /**
- * 未做完
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * <p>
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * 示例 4：
+ * <p>
+ * 输入：s = "aab" p = "c*a*b"
+ * 输出：true
+ * 解释：因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 示例 5：
+ * <p>
+ * 输入：s = "mississippi" p = "mis*is*p*."
+ * 输出：false
+ * <p>
+ * 解法： 动态规划
+ * dp[i][j] 表示s的前i个字符是否和p的前j个字符匹配，最后的结果是dp[s.length()][p.length()]
  */
 public class Hard10 {
     public static boolean nullOrEmpty(String str) {
@@ -27,7 +45,7 @@ public class Hard10 {
         char[] pChars = p.toCharArray();
         //初始化
         dp[0][0] = true;
-        for (int i = 1; i < pChars.length; i++) {
+        for (int i = 1; i <= pChars.length; i++) {
             int pos = i - 1;
             if (pChars[pos] == '*') {
                 dp[0][i] = dp[0][i - 2];
