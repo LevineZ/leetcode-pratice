7fb3f2f6fcd9e761d441b8088b35ce7d29c53a6c
boom <13132512292@163.com>
2021-10-08 18:03:51 +0800

    '先提交'

--- java/src/main/java/boom/hard/Hard10.java
+++ java/src/main/java/boom/hard/Hard10.java
@@ -18,32 +18,38 @@ public class Hard10 {
         }
         if (nullOrEmpty(p)) {
             return false;
-        }else if(nullOrEmpty(s)){
+        } else if (nullOrEmpty(s)) {
             return p.length() % 2 == 0 && p.replaceAll("\\*", "").length() == p.length() / 2;
         }
 
-        boolean[][] dp = new boolean[s.length()][p.length()];
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         char[] sChars = s.toCharArray();
         char[] pChars = p.toCharArray();
-        dp[0][0] = '.' == pChars[0] || sChars[0] == pChars[0];
-        if (p.length() > 1) {
-            dp[0][1] = '*' == pChars[1] && dp[0][0];
+        //初始化
+        dp[0][0] = true;
+        for (int i = 1; i < pChars.length; i++) {
+            int pos = i - 1;
+            if (pChars[pos] == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
         }
-        for (int i = 1; i < sChars.length; i++) {
-            for (int j = 1; j < p.length(); j++) {
-                if (pChars[j] != '*') {
-                    dp[i][j] = dp[i - 1][j - 1] && (pChars[j] == '.' || sChars[i] == pChars[j]);
+        for (int i = 1; i <= sChars.length; i++) {
+            int iPos = i - 1;
+            for (int j = 1; j <= pChars.length; j++) {
+                int jPos = j - 1;
+                if (pChars[jPos] != '*') {
+                    dp[i][j] = dp[i - 1][j - 1] && (pChars[jPos] == '.' || sChars[iPos] == pChars[jPos]);
                 } else {
-                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (sChars[i] == pChars[j - 1] || pChars[j - 1] == '.'));
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (sChars[iPos] == pChars[jPos - 1] || pChars[jPos - 1] == '.'));
                 }
             }
         }
-        return dp[s.length() - 1][p.length() - 1];
+        return dp[s.length()][p.length()];
     }
 
     public static void main(String[] args) {
-        String s = "aab";
-        String p = "c*a*b";
+        String s = "aa";
+        String p = "a*";
         System.out.println("result: " + isMatch(s, p));
     }
 }
